ead2afe58b321ecb7896c6057364a6d1be7bece3
Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
2026-04-03 18:08:45 +0300

    fix: api url

--- src/api/users.ts
+++ src/api/users.ts
@@ -4,8 +4,12 @@ type ApiResponse = {
   results: User[];
 };
 
+const API_URL = import.meta.env.DEV
+  ? "/api/?results=15"
+  : "https://randomuser.me/api/?results=15";
+
 export const fetchUsers = async (): Promise<ApiResponse> => {
-  const response = await fetch("/api/?results=15");
+  const response = await fetch(API_URL);
   if (!response.ok) {
     throw new Error("Failed to fetch users");
   }
